5e65da9b0cd6f2ef643c74bc6f98c6343c7b189f
Fabio Gibson <fabiogibson@hotmail.com>
2019-06-12 16:03:45 -0300

    simple refactoring

--- domain_reader/api/resources.py
+++ domain_reader/api/resources.py
@@ -1,72 +1,66 @@
 import falcon
 import json
 
-class DomainWriterResource():
 
-    def __init__(self, domain_writer):
-        self.domain_writer = domain_writer
+class BaseResource:
+    def __init__(self, controller):
+        self.controller = controller
 
-    def on_post(self, req, resp, map):
+    def bad_request(self, resp):
+        resp.status = falcon.HTTP_400
+
+    def internal_error(self, resp):
+        resp.status = falcon.HTTP_500
+
+    def json(self, data, resp, status_code=falcon.HTTP_200):
+        if not data:
+            return self.not_found()
+
+        resp.status = status_code
+        resp.body = json.dumps(data)
+
+    def accepted(self, resp, status_code=falcon.HTTP_200):
+        resp.status = status_code
+
+    def not_found(self, resp):
+        resp.status = falcon.HTTP_404
+
+
+class DomainWriterResource(BaseResource):
+    def on_post(self, req, resp, _map):
         process_memory_id = req.get_header('Instance-Id')
-        if process_memory_id == '':
-            resp.status = falcon.HTTP_400
-            return
 
-        ret = self.domain_writer.save_data(process_memory_id)
+        if not process_memory_id:
+            return self.bad_request()
 
-        if not ret:
-            resp.status = falcon.HTTP_500
-        else:
-            resp.status = falcon.HTTP_200
+        if not self.controller.save_data(process_memory_id):
+            return self.internal_error()
 
-class DomainResource():
+        return self.accepted()
 
-    def __init__(self, domain_reader):
-        self.domain_reader = domain_reader
 
-    def on_post(self, req, resp, _map, type, _filter):
-        if _map == '':
-            resp.status = falcon.HTTP_400
-            return
+class DomainReaderResource(BaseResource):
+    def on_post(self, req, resp, _map, _type, _filter):
+        if _map:
+            body = req.stream.read().decode('utf-8')
+            params = json.loads(body)
+            data = self.domain_reader.get_data(_map, _filter, params)
+            return self.json(data)
 
-        body = req.stream.read().decode('utf-8')
-        _params = json.loads(body)
-        data = self.domain_reader.get_data(_map, _filter, _params)
-
-        if data is None:
-            resp.status = falcon.HTTP_404
-        else:
-            resp.body = json.dumps(data)
-            resp.status = falcon.HTTP_200
+        return self.bad_request()
 
     def on_get(self, req, resp, _map, type, _filter):
-        if _map == '':
-            resp.status = falcon.HTTP_400
-            return
+        if  _map:
+            data = self.controller.get_data(_map, _filter, req.params)
+            return self.json(data)
 
-        __import__('ipdb').set_trace()
-        data = self.domain_reader.get_data(_map, _filter, req.params)
+        return self.bad_request()
 
-        if data is None:
-            resp.status = falcon.HTTP_404
-        else:
-            resp.body = json.dumps(data)
-            resp.status = falcon.HTTP_200
 
-class DomainHistoryResource():
-
-    def __init__(self, domain_reader):
-        self.domain_reader = domain_reader
-
+class DomainHistoryResource(BaseResource):
     def on_get(self, req, resp, _map, type, id):
-        if _map == '':
-            resp.status = falcon.HTTP_400
-            return
+        if _map:
+            data = self.domain_reader.get_data(_map, None, req.params, True)
+            return json(data)
 
-        data = self.domain_reader.get_data(_map, None, req.params, True)
-
-        if data is None:
-            resp.status = falcon.HTTP_404
-        else:
-            resp.body = json.dumps(data)
-            resp.status = falcon.HTTP_200
+        return self.bad_request()
--- domain_reader/app.py
+++ domain_reader/app.py
@@ -6,14 +6,26 @@ from platform_sdk.domain.writer import DomainWriter
 from .api import resources, settings
 
 
-api = falcon.API()
+from functools import partial
+
+
+class GlobalMiddleware:
+    def process_resource(self, req, resp, resource, params):
+        resource.bad_request = partial(resource.bad_request, resp=resp)
+        resource.internal_error = partial(resource.internal_error, resp=resp)
+        resource.accepted = partial(resource.accepted, resp=resp)
+        resource.json = partial(resource.json, resp=resp)
+        resource.not_found = partial(resource.not_found, resp=resp)
+
+
+api = falcon.API(middleware=[GlobalMiddleware()])
 api_version = 1
 
 domain_reader = DomainReader(settings.ORM, settings.DATABASE, settings.SCHEMA)
 domain_writer = DomainWriter(
     settings.ORM, settings.DATABASE, settings.PROCESS_MEMORY)
 
-domain_resource = resources.DomainResource(domain_reader)
+domain_resource = resources.DomainReaderResource(domain_reader)
 domain_history_resource = resources.DomainHistoryResource(domain_reader)
 domain_writer_resource = resources.DomainWriterResource(domain_writer)
 
