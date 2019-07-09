ef427316098231f7908551eb69c7b5a6927a547a
Bruno Leitão <bleitao@ons.org.br>
2019-07-09 19:20:24 -0300

    'text'

--- domain/reader/tests/test_query_parser.py
+++ domain/reader/tests/test_query_parser.py
@@ -1,3 +1,4 @@
+import ipdb
 from domain.reader import QueryParser
 
 
@@ -43,7 +44,7 @@ def test_parse_optional_parameter():
     assert query == 'col = val and id = %s'
 
 
-def test_remove_optional_parameter(): #
+def test_remove_optional_parameter():
     # mock
     sql_filter = 'col = val [and id = :id]'
     parameters = {}
@@ -60,6 +61,30 @@ def test_remove_optional_parameter(): #
 def test_parse_optional_filter_with_no_parameters():
     sql_filter = '[id in ($ids)]'
     parameters = {}
+
+    # act
+    parser = QueryParser(sql_filter)
+    query, params = parser.parse(parameters)
+
+    # assert
+    assert params == ()
+    assert query == ''
+
+
+def test_parse_sql():
+    sql_filter = '''(
+        :ExisteNomeCenario <> true [or nom_cenario in ($NomeCenario)] ) and 
+        (:ExisteIdUsina <> true [or id_usi in ($IdUsina)] ) 
+        [and id_tipocenario = :IdTipoCenario] 
+        [and id_situacaocenario = :IdSituacaoCenario] 
+        ORDER BY nom_cenario'''
+    parameters = {
+        'ExisteNomeCenario': 'False',
+        'NomeCenario': 'null',
+        'ExisteIdUsina': 'False',
+        'ExisteIdUsina': 'False',
+        'IdUsina': 'null',
+    }
 
     # act
     parser = QueryParser(sql_filter)
