07570f6e553d8dc39faf6e834fce484d05de9b64
Bruno Leitão <bleitao@ons.org.br>
2019-09-09 15:47:51 -0300

    'celery'

--- domain/writer/import_task.py
+++ domain/writer/import_task.py
@@ -3,6 +3,8 @@ from domain.api import settings
 from celery import Celery
 
 app = Celery(settings.CELERY['name'], broker=settings.CELERY['broker'])
+app.conf.task_default_queue = 'import_data'
+
 @app.task
 def import_data(entities):
     domain_writer = DomainWriter(settings.ORM, settings.DATABASE, settings.PROCESS_MEMORY)
