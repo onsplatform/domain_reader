3bad0c54d08d72b64b4e5d960152b03b3f914e56
Bruno Leitão <bleitao@ons.org.br>
2019-10-24 20:02:46 -0300

    refactoring

--- domain/reader/__init__.py
+++ domain/reader/__init__.py
@@ -13,30 +13,39 @@ class DomainReader(SQLExecutor):
         self.schema_api = SchemaApi(schema_settings)
         self._trace_local = lambda v, m: self._DomainReader__log.log(msg=f'{v}:{m}', level=autologging.TRACE)
 
-    def get_data(self, _map, _type, filter_name, params, history=False):
+    def get_data(self, _map, _type, filter_name, params):
         schema = self.schema_api.get_schema(_map, _type)
-
-        return self._get_data(schema, filter_name, params, history)
+        return self._get_data(schema, filter_name, params)
 
     def get_history_data(self, _map, _type, filter_name, params):
         schema = self.schema_api.get_schema(_map, _type)
-        ret = self._get_data(schema, filter_name, params, history=True, get_by_id=True)
-
-        return ret if ret else self._get_data(
-            schema, filter_name, params, history=False, count=False, get_by_id=True
-        )
+        ret = self._get_history_data(schema, filter_name, params)
+        if ret:
+            return ret
+        return self._get_data_by_id(schema, filter_name, params)
 
     def get_data_count(self, _map, _type, filter_name, params):
         schema = self.schema_api.get_schema(_map, _type)
-
-        return self._get_data(schema, filter_name, params, count=True)
-
-    def _get_data(self, schema, filter_name, params, history=False, count=False, get_by_id=False):
         if schema:
-            sql_properties = self._get_sql_properties(schema, filter_name, params, history, count, get_by_id)
-            ret = self._execute_query(**sql_properties)
+            sql_properties = self._get_sql_properties(schema, filter_name, params)
+            return self.execute_count_query(**sql_properties)
 
-            return ret if count else list(self._get_response_data(ret, schema['fields'], schema['metadata']))
+    def _get_data(self, schema, filter_name, params):
+        if schema:
+            ret = self.execute_data_query(schema, filter_name, params)
+            return list(self._get_response_data(ret, schema['fields'], schema['metadata']))
+
+    def _get_history_data(self, schema, filter_name, params):
+        if schema:
+            sql_properties = self._get_sql_properties(schema, filter_name, params)
+            ret = self.execute_data_query(**sql_properties)
+            return list(self._get_response_data(ret, schema['fields'], schema['metadata']))
+
+    def _get_data_by_id(self, schema, filter_name, params):
+        if schema:
+            sql_properties = self._get_sql_properties(schema, filter_name, params)
+            ret = self.execute_data_query_by_id(**sql_properties)
+            return list(self._get_response_data(ret, schema['fields'], schema['metadata']))
 
     @staticmethod
     def _get_response_data(entities, fields, metadata):
@@ -44,5 +53,4 @@ class DomainReader(SQLExecutor):
             for entity in entities:
                 dic = {field['alias']: getattr(entity, field['alias']) for field in fields}
                 dic['_metadata'] = {meta['alias']: getattr(entity, meta['alias']) for meta in metadata}
-
                 yield dic
--- domain/reader/sql_executor.py
+++ domain/reader/sql_executor.py
@@ -20,67 +20,89 @@ class SQLExecutor:
     def _create_db(self, orm, db_settings):
         self.db = orm.db_factory('postgres', **db_settings)()
 
-    def _execute_query(self, **options):  # pragma: no cover
-        try:
-            self.db.connect(reuse_if_open=True)
-            with self.db.atomic():
-                query = self._get_query(**options)
-                return query.count() if options.get('count') else query
-        except Exception as e:
-            self._trace_local('##### _execute_query ##### ERROR ', e)
-        finally:
-            self.db.close()
+    def execute_data_query_by_id(self, schema, _id):
+        model = self._get_model_from_schema(schema)
+        return self._execute_query(self._get_query_by_id(model, _id))
 
-    def _get_sql_properties(self, schema, filter_name, params, history, count=False, get_by_id=False):
-        model = self._get_model(schema['model'], schema['fields'] + schema['metadata'], history)
-        sql_filter = self._get_sql_filter(filter_name, schema['filters'], history, get_by_id)
-
+    def execute_data_query(self, schema, filter_name, params):
         page = params.get('page')
         branch = params.get('branch')
         page_size = params.get('page_size')
-        table = objects.get(schema, 'model.table')
-        sql_query = self._get_sql_query(sql_filter, {k: v for k, v in params.items() if k and v})
+        model = self._get_model_from_schema(schema)
+        sql_properties = self._get_sql_properties(schema['filters'], filter_name, params)
+        return self._execute_query(self._get_data_query(model, sql_properties, branch, page, page_size))
 
-        return dict(model=model, table=table, branch=branch,
-                    sql_query=sql_query, page=page, page_size=page_size,
-                    count=count, get_by_id=get_by_id)
+    def execute_history_data_query(self, schema, _id):
+        model = self._get_model_from_schema(schema)
+        return self._execute_query(self._get_query_by_id(model, _id))
 
-    def _get_query(self, **options):
+    def execute_count_query(self, schema, user_query_parameters, branch):
+        model = self._get_model_from_schema(schema)
+        query = self._get_count_query(model, user_query_parameters, branch).count()
+        return self._execute_query(query)
+
+    def _get_model_from_schema(self, schema):
+        return self._get_model(schema['model'], schema['fields'] + schema['metadata'])
+
+    def _get_sql_properties(self, filters, filter_name, params, history=False):
+        sql_filter = self._get_sql_filter(filter_name, filters, history)
+        params = {k: v for k, v in params.items() if k and v}
+        sql_query = self._get_sql_query(sql_filter, params)
+        return sql_query
+
+    def _get_query_by_id(self, model, _id):
+        query = model.build(self.db).select()
+        return query.where(SQL('id = %id', _id))
+
+    def _get_count_query(self, model, user_query_parameters, branch):
+        return self._get_base_query(model, user_query_parameters, branch).count()
+
+    def _get_data_query(self, model, user_query_parameters, branch, page, page_size):
+        base_query = self._get_base_query(model, user_query_parameters, branch)
+        if page and page_size:
+            base_query = base_query.paginate(int(page), int(page_size))
+        return base_query
+
+    def _get_base_query(self, model, user_query_parameters, branch):
         user_sql = ''
-        user_sql_query = options.get('sql_query')
+        query_params = ()
         query_not_deleted = self.QUERIES['not_deleted']
-        query = options.get('model').build(self.db).select()
 
-        if user_sql_query and user_sql_query['sql_query']:
-            user_sql = user_sql_query['sql_query']
-            query_params = user_sql_query['query_params']
+        import pdb;pdb.set_trace()
+        query_branch = self.QUERIES['where_branch'].format(table=model.table)
+        query = model.build(self.db).select()
 
-        query_branch = self.QUERIES['where_branch'].format(table=options.get('table'))
-        query_params = (options.get('branch'), options.get('branch'),) + query_params
-
+        if user_query_parameters and user_query_parameters['sql_query']:
+            user_sql = user_query_parameters['sql_query']
+            query_params = user_query_parameters['query_params']
+        query_params = (branch, branch,) + query_params
         if user_sql:
             query_not_deleted += ' AND '
-
-        if options.get('get_by_id'):
-            query_not_deleted = query_branch = ''
-            query_params = user_sql_query['query_params']
-
         query = query.where(SQL(
             query_branch + query_not_deleted + user_sql,
             query_params
         ))
-
-        if options.get('page') and options.get('page_size') and not options.get('count'):
-            query = query.paginate(int(options.get('page')), int(options.get('page_size')))
-
         return query
+
+    def _execute_query(self, query):  # pragma: no cover
+        try:
+            self.db.connect(reuse_if_open=True)
+            with self.db.atomic():
+                return query
+        except Exception as e:
+            self._trace_local('##### _execute_query ##### ERROR ', e)
+        finally:
+            self.db.close()
 
     @staticmethod
     def _get_sql_query(sql_filter, params):
         if sql_filter and params:
             parser = QueryParser(sql_filter)
             query, params = parser.parse(params)
-            return dict(sql_query=query, query_params=params)
+            return {
+                'sql_query': query,
+                'query_params': params
+            }
 
     @staticmethod
     def _get_fields(fields):
@@ -91,8 +113,8 @@ class SQLExecutor:
         return RemoteMap(model['name'], model['table'], self._get_fields(fields), self.orm, history)
 
     @staticmethod
-    def _get_sql_filter(filter_name, filters, history, get_by_id=False):
-        if history or get_by_id:
+    def _get_sql_filter(filter_name, filters, history):
+        if history:
             return 'id = :id'
         if filters and filter_name:
             return next(f['expression'] for f in filters if f['name'] == filter_name)
